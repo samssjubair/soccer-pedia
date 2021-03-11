f3c320a72adf648975f565e1b4ba9560e7193032
samssjubair <samssjubair@gmail.com>
2021-03-11 18:22:22 +0600

    responsiveness added

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 It's a web application which is built using React Library along with Material UI framework.
 You can learn about your favorite English Premier League clubs and know their history, trophies, players and many other info easily. Feel free to explore your soccer world!
 
-### Netlify live website link: https://soccer-pedia-epl.netlify.app/
+### Netlify live website link: 
+https://soccer-pedia-epl.netlify.app/
--- src/Components/ShortDetail/ShortDetail.js
+++ src/Components/ShortDetail/ShortDetail.js
@@ -1,4 +1,4 @@
-import { Box } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 import React from 'react';
 import maleImage from '../../Assets/Images/male.png';
 import femaleImage from '../../Assets/Images/lieke.jpg';
@@ -11,18 +11,18 @@ import { faMars } from '@fortawesome/free-solid-svg-icons'
 const ShortDetail = (props) => {
     const {intFormedYear ,strTeam,strGender,strCountry,strSport}= props.detail;
     return (
-        <Box style={{backgroundColor: '#FC4D57',color: 'white',margin: '30px 0px'}} borderRadius={10} alignItems='center' padding='10px 10px 10px 20px' textAlign='left' justifyContent='space-between' display='flex'>
-            <div>
+        <Grid container style={{backgroundColor: '#FC4D57',color: 'white',margin: '30px 0px',padding:'20px', textAlign:'left',borderRadius: '10px',display:'flex',alignItems:'center', justifyContent:'space-between'}}   >
+            <Grid style={{marginBottom:'20px'}} item sm={12} md={8}>
                 <h1>{strTeam}</h1> <br/> 
                 <p><FontAwesomeIcon fixedWidth  size='xs' icon={faMapMarkerAlt} /> &nbsp; Founded: {intFormedYear}</p>
                 <p><FontAwesomeIcon fixedWidth  size='xs' icon={faFlag} /> &nbsp;Country: {strCountry}</p>
                 <p><FontAwesomeIcon fixedWidth  size='xs' icon={faFutbol} /> &nbsp;Sport: {strSport}</p>
                 <p><FontAwesomeIcon fixedWidth  size='xs' icon={faMars} /> &nbsp;Gender: {strGender}</p>
-            </div>
-            <div>
-                <img style={{borderRadius: '10px',height: '200px'}} src={maleImage} alt=""/>
-            </div>
-        </Box>
+            </Grid>
+            <Grid item sm={12} md={4}>
+                <img style={{borderRadius: '10px',height: '200px',width:'100%'}} src={maleImage} alt=""/>
+            </Grid>
+        </Grid>
     );
 };
 
