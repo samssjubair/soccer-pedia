0adf0bfa981e2d40edcfb7d6706c7f2f337fd1e8
samssjubair <samssjubair@gmail.com>
2021-03-11 14:18:40 +0600

    social button added

--- src/Components/DetailBody/DetailBody.js
+++ src/Components/DetailBody/DetailBody.js
@@ -1,20 +1,94 @@
-import { Box } from '@material-ui/core';
-import React from 'react';
-import ShortDetail from '../ShortDetail/ShortDetail';
-import './DetailBody.css'
+import { Box } from "@material-ui/core";
+import React from "react";
+import ShortDetail from "../ShortDetail/ShortDetail";
+import "./DetailBody.css";
+import {
+  faTwitter,
+  faFacebook,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import { lightBlue,red, deepPurple } from "@material-ui/core/colors";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: 'center',
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  lightBlue: {
+    color: theme.palette.getContrastText(lightBlue[200]),
+    backgroundColor: lightBlue[200],
+  },
+  red: {
+    color: theme.palette.getContrastText(red[500]),
+    backgroundColor: red[500],
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[900]),
+    backgroundColor: deepPurple[900],
+  },
+}));
 
 const DetailBody = (props) => {
-    const {strTeam,intFormedYear,strFacebook,strYoutube,strTwitter,strGender,strCountry,strSport,strDescriptionEN,strStadiumDescription,strTeamJersey}=props.club;
-    return (
-        <Box width="75%" style={{margin: '0 auto',textAlign: 'center'}}>
-            <div>
-                <ShortDetail detail={{strTeam,intFormedYear,strGender,strCountry,strSport}}></ShortDetail>
-                <p className="justify"> <strong>Club Description:</strong>  {strDescriptionEN}</p> <br/>
-                <p className="justify"><strong>Stadium Description:</strong> {strStadiumDescription}</p>
-            </div>
-        </Box>
-    );
+  const classes = useStyles();
+
+  const {
+    strTeam,
+    intFormedYear,
+    strFacebook,
+    strYoutube,
+    strTwitter,
+    strGender,
+    strCountry,
+    strSport,
+    strDescriptionEN,
+    strStadiumDescription,
+    strTeamJersey,
+  } = props.club;
+  return (
+    <Box width="75%" style={{ margin: "0 auto", textAlign: "center" }}>
+      <div>
+        <ShortDetail
+          detail={{ strTeam, intFormedYear, strGender, strCountry, strSport }}
+        ></ShortDetail>
+        <p className="justify">
+          {" "}
+          <strong>Club Description:</strong> {strDescriptionEN}
+        </p>{" "}
+        <br />
+        <p className="justify">
+          <strong>Stadium Description:</strong> {strStadiumDescription}
+        </p>
+      </div>
+      <div>
+        <div style={{marginTop: '20px'}} className={classes.root}>
+            <a rel={'external'} target="_blank" href={`http://${strTwitter}`}>
+                <Avatar  className={classes.blue}><FontAwesomeIcon icon={faTwitter} /></Avatar>  
+            </a>
+            <a rel={'external'} target="_blank" href={`http://${strFacebook}`}>
+                <Avatar className={classes.purple}><FontAwesomeIcon icon={faFacebook} /></Avatar>
+            </a>
+            <a rel={'external'} target="_blank" href={`http://${strYoutube}`}>
+                <Avatar className={classes.red}><FontAwesomeIcon icon={faYoutube} /></Avatar>  
+            </a>
+          
+          
+          
+        </div>
+
+        
+
+        
+        
+      </div>
+    </Box>
+  );
 };
 
 export default DetailBody;
--- src/Components/ShortDetail/ShortDetail.js
+++ src/Components/ShortDetail/ShortDetail.js
@@ -1,7 +1,12 @@
 import { Box } from '@material-ui/core';
 import React from 'react';
-import maleImage from '../../Assets/Images/male.png'
-import femaleImage from '../../Assets/Images/lieke.jpg'
+import maleImage from '../../Assets/Images/male.png';
+import femaleImage from '../../Assets/Images/lieke.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { faFlag } from '@fortawesome/free-solid-svg-icons'
+import { faFutbol } from '@fortawesome/free-solid-svg-icons'
+import { faMars } from '@fortawesome/free-solid-svg-icons'
 
 const ShortDetail = (props) => {
     const {intFormedYear ,strTeam,strGender,strCountry,strSport}= props.detail;
@@ -9,13 +14,13 @@ const ShortDetail = (props) => {
         <Box style={{backgroundColor: '#FC4D57',color: 'white',margin: '30px 0px'}} borderRadius={10} alignItems='center' padding='10px 10px 10px 20px' textAlign='left' justifyContent='space-between' display='flex'>
             <div>
                 <h1>{strTeam}</h1> <br/> 
-                <p>Founded: {intFormedYear}</p>
-                <p>Country: {strCountry}</p>
-                <p>Sport: {strSport}</p>
-                <p>Gender: {strGender}</p>
+                <p><FontAwesomeIcon fixedWidth  size='xs' icon={faMapMarkerAlt} /> &nbsp; Founded: {intFormedYear}</p>
+                <p><FontAwesomeIcon fixedWidth  size='xs' icon={faFlag} /> &nbsp;Country: {strCountry}</p>
+                <p><FontAwesomeIcon fixedWidth  size='xs' icon={faFutbol} /> &nbsp;Sport: {strSport}</p>
+                <p><FontAwesomeIcon fixedWidth  size='xs' icon={faMars} /> &nbsp;Gender: {strGender}</p>
             </div>
             <div>
-                <img style={{borderRadius: '10px'}} src={femaleImage} alt=""/>
+                <img style={{borderRadius: '10px',height: '200px'}} src={maleImage} alt=""/>
             </div>
         </Box>
     );
