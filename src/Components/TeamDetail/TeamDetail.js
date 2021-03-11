ad0428f6f34ba84950aa4dc3a5c39ef58b8d2daa
samssjubair <samssjubair@gmail.com>
2021-03-11 14:25:19 +0600

    netlify bug fixed

--- src/Components/TeamDetail/TeamDetail.js
+++ src/Components/TeamDetail/TeamDetail.js
@@ -10,7 +10,7 @@ const TeamDetail = () => {
         fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${teamId}`)
         .then(res=>res.json())
         .then(data=>setClub(data.teams[0]))
-    },[])
+    },[teamId])
     const {strStadiumThumb,strTeamBadge}=club;
     return (
         <div style={{backgroundColor: '#0a0e2a',color: 'white'}}>
